cce4043605df55a0b326ea6622a0d55dc9fd57df
Z0pyrus <mail@zopyrus.de>
2024-03-12 16:38:36 +0100

    Timer is going now and time is displayed

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define __AVR_ATmega48a__
+#define __AVR_ATmega48A__
 
 #define F_CPU 1000000UL
 #include <avr/io.h>
@@ -13,7 +13,7 @@
 volatile uint8_t hour = 0;
 volatile uint8_t minute = 0;
 
-volatile uint16_t ms = 0;
+volatile uint16_t second = 0;
 /////////////////////////////////////////////
 //Buttons
 
@@ -49,6 +49,12 @@ const Pin hourLedPins[] = {
 };
 
 const size_t numHourLedPins = sizeof(hourLedPins) / sizeof(hourLedPins[0]);
+
+// Method Declaration
+void displayTime(uint8_t hour, uint8_t minute);
+void setEverythingOff();
+
+
 /////////////////////////////////
 int main() {
 	// Setze alle Pins von Port C als Ausgänge
@@ -91,7 +97,13 @@ int main() {
 
 	while (1)
 	{	
-		asm("nop");		
+		_delay_ms(18);
+		displayTime(hour, 0);
+		_delay_us(90);
+		setEverythingOff();
+		displayTime(0, minute);
+		_delay_us(90);
+		setEverythingOff();
 	}
 }
 
@@ -113,35 +125,49 @@ ISR(INT0_vect){
 
 //Timer0 Interrupt
 ISR(TIMER2_COMPA_vect) {
-	PORTB |= (1<<PB0);
-	_delay_ms(1);
-	PORTB &= ~(1<<PB0);
-}
-
-
-/*
-#include <avr/io.h>
-
-// Reihenfolge der Pins: PIND1, PIND4, PIND3, PIND0, PIND2, PIND5
-void setPins(uint8_t mask)
-{
-	// Erstelle ein Array, das die Reihenfolge der Pins widerspiegelt
-	uint8_t pinOrder[] = {PIND1, PIND4, PIND3, PIND0, PIND2, PIND5};
-	
-	// Gehe durch die Pins in der gew�nschten Reihenfolge
-	for (int i = 0; i < 6; i++)
-	{
-		if (mask & (1 << i)) // �berpr�fe, ob das entsprechende Bit in der Maske gesetzt ist
-		{
-			// Setze den Pin auf HIGH
-			PORTD |= (1 << pinOrder[i]);
-		}
-		else
-		{
-			// Setze den Pin auf LOW
-			PORTD &= ~(1 << pinOrder[i]);
+	second++;
+	if(second == 60){
+		second = 0;
+		minute++;
+		if(minute == 60){
+			minute = 0;
+			hour++;
+			if(hour == 24){
+				hour = 0;
+			}
 		}
 	}
 }
 
-*/
+//Method to let the LEDs display the time
+void displayTime(uint8_t hour, uint8_t minute){
+	for (size_t i = 0; i < numMinLedPins; i++)
+	{
+		if (minute & (1 << i))
+		{
+			*minLedPins[i].port |= (1 << minLedPins[i].pin);
+		}
+		else
+		{
+			*minLedPins[i].port &= ~(1 << minLedPins[i].pin);
+		}
+	}
+	
+	for (size_t i = 0; i < numHourLedPins; i++)
+	{
+		if (hour & (1 << i))
+		{
+			*hourLedPins[i].port |= (1 << hourLedPins[i].pin);
+		}
+		else
+		{
+			*hourLedPins[i].port &= ~(1 << hourLedPins[i].pin);
+		}
+	}
+}
+
+void setEverythingOff() {
+	PORTC &= ~0b00111111;
+	PORTD &= ~0b11000000;
+	PORTB &= ~0b00000111;
+}
